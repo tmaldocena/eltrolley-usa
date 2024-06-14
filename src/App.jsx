c3fd5df7b7152a43245462447c766d48089543bf
Tomas Maldocena <104042642+tmaldocena@users.noreply.github.com>
2024-06-14 14:35:29 -0300

    building site

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,10 @@ import Building from './assets/Building.jsx'
 function App() {
 
   return (
-    <div className="w-full h-screen flex flex-row justify-evenly items-center text-white">
-      <div className='flex flex-col justify-center items-start'>
+    <div className="w-full h-screen flex lg:flex-row md:flex-row flex-col justify-evenly items-center text-white">
+      <div className='flex flex-col justify-center lg:items-start md:items-start items-center'>
         <img src='./logo-trolley-usa1.png' alt='Trolley USA logo' className='w-32' />
-        <h1 className="text-5xl my-4">
+        <h1 className="text-5xl my-4 lg:text-start md:text-start text-center">
           We are <b className='text-[#C41F25]'>Almost</b> there!
         </h1>
         <p>Stay tuned for something amazing!!!</p>
